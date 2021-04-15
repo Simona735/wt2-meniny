0196cb5f781e76db770aec935dd00e3f5797144b
Simona735 <72383935+Simona735@users.noreply.github.com>
2021-04-15 16:32:20 +0200

    CZ and SK sviatky corrected

--- api/CZsviatky/index.php
+++ api/CZsviatky/index.php
@@ -3,18 +3,13 @@ require_once "../../Database.php";
 header('Content-Type: application/json');
 $conn = (new Database())->getConnection();
 
-$stmt = $conn->query("SELECT days.day as day, days.month as month, records.value as value FROM records JOIN days ON days.id=records.day_id JOIN countries ON countries.code='CZ' WHERE records.type='holiday' ORDER BY days.month, days.day");
-
+$stmt = $conn->query("SELECT days.day as day, days.month as month, records.value as value FROM records JOIN days ON days.id=records.day_id JOIN countries ON countries.id=records.country_id WHERE records.type='holiday' AND countries.code='CZ' ORDER BY days.month, days.day");
 
 $CZsviatky = [];
 while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
     array_push($CZsviatky, [$row["day"].".".$row["month"].".", $row["value"]]);
 }
-
-
-
-//var_dump($CZsviatky);
 echo json_encode($CZsviatky, JSON_UNESCAPED_UNICODE);
 
 ?>
--- api/SKsviatky/index.php
+++ api/SKsviatky/index.php
@@ -1,1 +1,13 @@
 <?php
+require_once "../../Database.php";
+header('Content-Type: application/json');
+$conn = (new Database())->getConnection();
+
+$stmt = $conn->query("SELECT days.day as day, days.month as month, records.value as value FROM records JOIN days ON days.id=records.day_id JOIN countries ON countries.id=records.country_id WHERE records.type='holiday' AND countries.code='SK' ORDER BY days.month, days.day");
+
+$SKsviatky = [];
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    array_push($SKsviatky, [$row["day"] . "." . $row["month"] . ".", $row["value"]]);
+}
+echo json_encode($SKsviatky, JSON_UNESCAPED_UNICODE);
+
