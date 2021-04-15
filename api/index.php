288273cc7328d69a58458c48ac9dba9499790b07
Simona735 <72383935+Simona735@users.noreply.github.com>
2021-04-15 23:42:54 +0200

    responses description missing

--- api/index.php
+++ api/index.php
@@ -2,45 +2,42 @@
 require_once "../Database.php";
 $conn = (new Database())->getConnection();
 
-var_dump($_GET);
-
 $codes = ["SK", "CZ", "AT", "HU", "PL",];
 
-if(isset($_GET["code"])){
+if(isset($_GET["code"]) && isset($_GET["name"])){
     $country_code = strtoupper($_GET["code"]);
-
+    $name = ucfirst(strtolower($_GET["name"]));
     if(!in_array($country_code, $codes)){
         echo "invalid state";
         return;
     }
 
-    if(isset($_GET["name"])){
-        $name = ucfirst(strtolower($_GET["name"]));
-
-        $stmt = $conn->query("SELECT days.day as day, days.month as month FROM records JOIN countries ON records.country_id=countries.id JOIN days ON records.day_id=days.id WHERE countries.code='".$country_code."' and records.value='".$name."'");
-        $date = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($date == null){
-            echo "nobody found";
-        }else{
-            echo $date["day"].".".$date["month"].".";
-        }
-    }else if(isset($_GET["date"])){
-        $day = substr($_GET["date"], 0, 2);
-        $month = substr($_GET["date"], 2, 2);
-        $stmt = $conn->query("SELECT records.type as type, records.value as value FROM records JOIN countries ON records.country_id=countries.id JOIN days ON days.id=records.day_id WHERE days.day=".$day." AND days.month=".$month." AND countries.code='".$country_code."'");
-
-        $records = [];
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-            if (!strcmp($row["type"],"name" )){
-                array_push($records, $row["value"]);
-            }
-        }
-        if (count($records) == 0){
-            array_push($records, "Nikto nemá meniny");
-        }
-        echo json_encode($records, JSON_UNESCAPED_UNICODE);
+    $stmt = $conn->query("SELECT days.day as day, days.month as month FROM records JOIN countries ON records.country_id=countries.id JOIN days ON records.day_id=days.id WHERE countries.code='".$country_code."' and records.value='".$name."'");
+    $date = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($date == null){
+        echo "nobody found";
+    }else{
+        echo $date["day"].".".$date["month"].".";
     }
+
+}else if(isset($_GET["date"])){
+    $day = substr($_GET["date"], 0, 2);
+    $month = substr($_GET["date"], 2, 2);
+    $stmt = $conn->query("SELECT code, title, value FROM records JOIN days ON records.day_id=days.id JOIN countries ON records.country_id=countries.id WHERE records.type='name' days.day=".$day." AND days.month=".$month);
+
+    $records = [];
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+    {
+        if (array_key_exists($row["code"],$records)){
+            array_push($records[$row["code"]], $row["value"]);
+        }else{
+            $records[$row["code"]] = [$row["value"]];
+        }
+    }
+    if (count($records) == 0){
+        array_push($records, "Nikto nemá meniny");
+    }
+    echo json_encode($records, JSON_UNESCAPED_UNICODE);
 
 }else if (isset($_POST["name"]) && isset($_POST["date"])){
     $stmt = $conn->query("SELECT id FROM countries WHERE countries.code='SK'");
@@ -54,8 +51,8 @@ if(isset($_GET["code"])){
 
     $stmt = $conn->query("INSERT INTO `records`(`day_id`, `country_id`, `type`, `value`) VALUES (".$dayId.",".$SKid.",'name','".$_POST["name"]."')");
     //echo $dayId.",".$SKid.",'name',".$_POST["name"];
-    echo "success";
+    echo "successfully added";
 } else{
-    echo "you fucked up";
+    echo "something went wrong";
 }
 
--- index.php
+++ index.php
@@ -30,14 +30,213 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 <div class="container">
     <main>
         <div class="py-5 text-center">
-            <h2>REST API form</h2>
-            <p class="lead">Try it out!</p>
+            <h2 class="">REST API Dokumentácia</h2>
+<!--            <p class="lead">Try it out!</p>-->
+            <button id="docButton" class="w-25 mt-3 btn btn-primary" >Dokumentácia</button>
         </div>
 
+        <div id="doc" class="row g-5 justify-content-center">
+            <div class="col-md-8">
+                <div class="card mt-3">
+                    <h5 class="card-header">České sviatky</h5>
+                    <div class="card-body">
+                        <h6 class="card-title text-primary">PATH</h6>
+                        <p class="card-text">GET /api/CZsviatky</p>
+                        <h6 class="card-title mt-4 text-primary">DECRIBTION</h6>
+                        <p class="card-text">získa zoznam všetkých sviatkov v Čechách spolu s dňom, na ktorý tieto sviatky pripadajú</p>
+                        <h6 class="card-title mt-4 text-primary">RESPONSES</h6>
+                        <p class="card-text">content</p>
+                        <a href="#specialsForm" class="btn btn-primary">Vyskúšaj</a>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <h5 class="card-header ">Slovenské sviatky</h5>
+                    <div class="card-body">
+                        <h6 class="card-title text-primary">PATH</h6>
+                        <p class="card-text">GET /api/SKsviatky</p>
+                        <h6 class="card-title mt-4 text-primary">DECRIBTION</h6>
+                        <p class="card-text">získa zoznam všetkých sviatkov na Slovensku spolu s dňom, na ktorý tieto sviatky pripadajú</p>
+                        <h6 class="card-title mt-4 text-primary">RESPONSES</h6>
+                        <p class="card-text">content</p>
+                        <a href="#specialsForm" class="btn btn-primary">Vyskúšaj</a>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <h5 class="card-header ">Slovenské pamätné dni</h5>
+                    <div class="card-body">
+                        <h6 class="card-title text-primary">PATH</h6>
+                        <p class="card-text">GET /api/SKdni</p>
+                        <h6 class="card-title mt-4 text-primary">DECRIBTION</h6>
+                        <p class="card-text">získa zoznam všetkých pamätných dní na Slovensku spolu s dňom, na ktorý tieto dni pripadajú</p>
+                        <h6 class="card-title mt-4 text-primary">RESPONSES</h6>
+                        <p class="card-text">content</p>
+                        <a href="#specialsForm" class="btn btn-primary">Vyskúšaj</a>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <h5 class="card-header ">Kto má meniny podľa dátumu</h5>
+                    <div class="card-body">
+                        <h6 class="card-title text-primary">PATH</h6>
+                        <p class="card-text">GET /api/{date}</p>
+                        <h6 class="card-title mt-4 text-primary">DECRIBTION</h6>
+                        <p class="card-text">na základe zadaného dátumu získa informáciu, kto má v daný deň meniny na Slovensku, resp. v niektorom inom uvedenom štáte</p>
+                        <h6 class="card-title mt-4 text-primary">REQUEST PARAMETERS</h6>
+                        <hr>
+                        <table class="table table-borderless table-sm">
+                            <tbody>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">date: string</th>
+                                <td >Date</td>
+                            </tr>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">format:</th>
+                                <td>ddmm</td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6"><span class="badge bg-danger">required</span></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6">in path</td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                        <h6 class="card-title mt-4 text-primary">EXAMPLE</h6>
+                        <p class="card-text">/api/2804</p>
+                        <h6 class="card-title mt-4 text-primary">RESPONSES</h6>
+                        <p class="card-text">content</p>
+                        <a href="#whoForm" class="btn btn-primary">Vyskúšaj</a>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <h5 class="card-header">Kedy má osoba meniny v danom štáte</h5>
+                    <div class="card-body">
+                        <h6 class="card-title text-primary">PATH</h6>
+                        <p class="card-text">GET /api/{code}/{name}</p>
+                        <h6 class="card-title mt-4 text-primary">DECRIBTION</h6>
+                        <p class="card-text">na základe uvedeného mena a štátu získa informáciu, kedy má osoba s týmto menom meniny v danom štáte</p>
+                        <h6 class="card-title mt-4 text-primary">REQUEST PARAMETERS</h6>
+                        <hr>
+                        <table class="table table-borderless table-sm">
+                            <tbody>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">code: string</th>
+                                <td >State code</td>
+                            </tr>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">format:</th>
+                                <td>XY</td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6"><span class="badge bg-danger">required</span></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6">in path</td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                        <table class="table table-borderless table-sm">
+                            <tbody>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">name: string</th>
+                                <td >Name</td>
+                            </tr>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">format:</th>
+                                <td>characters only, at least one</td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6"><span class="badge bg-danger">required</span></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6">in path</td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                        <h6 class="card-title mt-4 text-primary">EXAMPLE</h6>
+                        <p class="card-text">/api/SK/2804</p>
+                        <h6 class="card-title mt-4 text-primary">RESPONSES</h6>
+                        <p class="card-text">content</p>
+                        <a href="#whenForm" class="btn btn-primary">Vyskúšaj</a>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <h5 class="card-header">Vlož nové meno k určitému dňu</h5>
+                    <div class="card-body">
+                        <h6 class="card-title text-primary">PATH</h6>
+                        <p class="card-text">POST /api</p>
+                        <h6 class="card-title mt-4 text-primary">DECRIBTION</h6>
+                        <p class="card-text">vloží nové meno do kalendára k určitému dňu</p>
+                        <h6 class="card-title mt-4 text-primary">REQUEST PARAMETERS</h6>
+                        <hr>
+                        <table class="table table-borderless table-sm">
+                            <tbody>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">date: string</th>
+                                <td >Date</td>
+                            </tr>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">format:</th>
+                                <td>ddmm</td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6"><span class="badge bg-danger">required</span></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6">in path</td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                        <table class="table table-borderless table-sm">
+                            <tbody>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">name: string</th>
+                                <td >Name</td>
+                            </tr>
+                            <tr>
+                                <th class="text-end pe-4 col-md-6">format:</th>
+                                <td>characters only, at least one</td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6"><span class="badge bg-danger">required</span></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td class="text-end pe-4 col-md-6">in path</td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                        <h6 class="card-title mt-4 text-primary">RESPONSES</h6>
+                        <p class="card-text">content</p>
+                        <a href="#addForm" class="btn btn-primary">Vyskúšaj</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+
+
+        <div class="py-5 text-center">
+            <h2>REST API formuláre</h2>
+            <p class="lead">Vyskúšaj si to!</p>
+        </div>
         <div class="row g-5 justify-content-center">
             <div class="col-md-8">
                 <h4 class="mb-3">Získaj zoznam</h4>
-                <form class="mb-5" id="specialsForm">
+                <form class="mb-2" id="specialsForm">
                     <div class="col-md-5">
                         <label for="specials" class="form-label">Získaj zoznam sviatkov alebo dní</label>
                         <select class="form-select" name="specials" id="specials" required>
@@ -49,24 +248,15 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
                     </div>
                     <button class="w-25 mt-3 btn btn-primary" type="submit">Submit</button>
                 </form>
+                <div class="form-floating mb-5">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="listArea"></textarea>
+                    <label for="listArea">Result</label>
+                </div>
                 <hr>
 
                 <h4 class="mb-3">Kto má meniny?</h4>
-                <form class="mb-5" id="whoForm">
+                <form class="mb-3" id="whoForm">
                     <div class="row g-3">
-                        <div class="col-md-6">
-                            <label for="whoCountry" class="form-label">Krajina</label>
-                            <select class="form-select" name="whoCountry" id="whoCountry" required>
-                                <option value="">Choose...</option>
-                                <?php
-                                foreach ($countries as $country){?>
-                                    <option value="<?php echo $country[1] ?>"><?php echo $country[0]?></option>
-                                    <?php
-                                }
-                                ?>
-
-                            </select>
-                        </div>
                         <div class="col-md-6">
                             <label for="whoDate" class="form-label">Dátum</label>
                             <input type="date" class="form-control" name="whoDate" id="whoDate" placeholder="" value="" required>
@@ -75,10 +265,14 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 
                     <button class="w-25 mt-3 btn btn-primary" type="submit">Submit</button>
                 </form>
+                <div class="form-floating mb-5">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="whoArea"></textarea>
+                    <label for="whoArea">Result</label>
+                </div>
                 <hr>
 
                 <h4 class="mb-3">Kedy má meniny ...?</h4>
-                <form class="mb-5" id="whenForm">
+                <form class="mb-3" id="whenForm">
                     <div class="row g-3">
                         <div class="col-md-6">
                             <label for="whenCountry" class="form-label">Krajina</label>
@@ -100,10 +294,14 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
                     </div>
                     <button class="w-25 mt-3 btn btn-primary " type="submit">Submit</button>
                 </form>
+                <div class="form-floating mb-5">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="whenArea"></textarea>
+                    <label for="whenArea">Result</label>
+                </div>
                 <hr>
 
                 <h4 class="mb-3">Pridaj nové meno</h4>
-                <form class="mb-5" id="addForm">
+                <form class="mb-3" id="addForm">
                     <div class="row g-3">
                         <div class="col-md-6">
                             <label for="newDate" class="form-label">Dátum</label>
@@ -116,6 +314,10 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
                     </div>
                     <button class="w-25 mt-3 btn btn-primary " type="submit">Submit</button>
                 </form>
+                <div class="form-floating mb-5">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="addArea"></textarea>
+                    <label for="addArea">Result</label>
+                </div>
             </div>
         </div>
     </main>
@@ -128,21 +330,28 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
 
 <script>
+    $('#docButton').click(function() {
+        $('#doc').toggle();
+    });
+
+
     $( "#specialsForm" ).submit(function( event ) {
         event.preventDefault();
         $.get(
             "api/"+getValue("specials"),
             function(result) {
-                console.log(result);
+                $( "#listArea" ).val(JSON.stringify(result));
             }
         );
     });
     $( "#whoForm" ).submit(function( event ) {
         event.preventDefault();
         $.get(
-            "api/"+getValue("whoCountry") +"/"+formatDateById("whoDate"),
+            "api/"+formatDateById("whoDate"),
             function(result) {
                 console.log(result);
+                console.log(typeof result);
+                $( "#whoArea" ).val(result);
             }
         );
 
@@ -152,7 +361,7 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
         $.get(
             "api/"+ getValue("whenCountry") +"/"+ getValue("whenName"),
             function(result) {
-                console.log(result);
+                $( "#whenArea" ).val(result);
             }
         );
     });
@@ -162,7 +371,7 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
             "api/",
             {date: formatDateById("newDate"), name: getValue("newName")},
             function(result){
-                console.log(result);
+                $( "#addArea" ).val(result);
             }
         );
     });
